6d2bb88d4aa7b181d40a286292a8937c561c89fb
Placid Masvidal <pmasvidal@hillogy.com>
2020-12-18 11:17:13 +0100

    Initial commit, setting up project

---
+++ immfly-challenge/src/main/java/com/immfly/immflychallenge/ImmflyChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.immfly.immflychallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmflyChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmflyChallengeApplication.class, args);
+	}
+
+}
---
+++ immfly-challenge/src/test/java/com/immfly/immflychallenge/ImmflyChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.immfly.immflychallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmflyChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
